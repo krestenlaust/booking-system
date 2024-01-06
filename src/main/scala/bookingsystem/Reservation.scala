9a9b4777d76a3cdcb0a4a2a2daa71e2340253f63
Kresten Laust <krestenlaust@gmail.com>
2024-01-06 01:03:24 +0100

    Reformatted file

--- src/main/scala/bookingsystem/Reservation.scala
+++ src/main/scala/bookingsystem/Reservation.scala
@@ -2,22 +2,28 @@ package bookingsystem
 import com.github.nscala_time.time.Imports.*
 
 class Reservation(val startTime: LocalDateTime, val duration: Duration) extends TimeSlot:
-  override def overlaps(other: TimeSlot): Boolean = ???
+  override def overlaps(other: TimeSlot): Boolean =
+    ???
 
-  override def covers(other: TimeSlot): Boolean = ???
+  override def covers(other: TimeSlot): Boolean =
+    ???
 
-  override def startDate: LocalDateTime = startTime + duration
+  override def startDate: LocalDateTime =
+    startTime
 
-  override def endDate: LocalDateTime = startTime + duration
+  override def endDate: LocalDateTime =
+    startTime + duration
 
-  override def available: Boolean = ???
+  override def available: Boolean =
+    ???
 
   /**
    * Updates the time used by the group
    * when the reservation is approved.
    * @return
    */
-  def approve = ??? // No idea
+  def approve =
+    ??? // No idea
 
-  override def toString: String = ???
-
+  override def toString: String =
+    ???
